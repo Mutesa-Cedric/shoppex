76f633489a803e644261fb309c4f73004b6885d1
Mutesa-Cedric <mutesacedric@gmail.com>
2022-07-06 12:06:33 +0200

    update : profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
 
 function profile() {
-  return (
-    <div>profile</div>
-  )
-}
+    let logout = () => {
 
+    }
+    return (
+        <div className='w-full h-screen'>
+            <div>
+                <h1>Your Profile</h1>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                <h1>
+                    your recent activities
+                </h1>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                <h1>
+                    statistics
+                </h1>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                <button className='px-4 py-2 bg-red-500' onClick={() => logout()}>
+                    logout
+                </button>
+            </div>
+        </div>
+    )
+}
 export default profile
