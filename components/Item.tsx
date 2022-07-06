9d79469ff1e40ba6d4d2e46df76070878e013ec7
MUTESA Cedric <mutesacedric@gmail.com>
2022-07-05 22:51:59 -0700

    Update Item.tsx

--- components/Item.tsx
+++ components/Item.tsx
@@ -8,8 +8,7 @@ function Item() {
              justify-between rounded-2xl shadow-sm">
             <p className="font-medium">Chicken 1KG</p>
             <AiOutlinePlus size={20} color={hover ? "#F9A109" : ""} onMouseOver={() => setHover(true)}
-                onMouseLeave={() => setHover(false)} className="transition-all duration-100 hover:scale-125"
-                className="cursor-pointer"/>
+                onMouseLeave={() => setHover(false)} className="transition-all duration-100 hover:scale-125 cursor-pointer"/>
         </div>
     )
 }
