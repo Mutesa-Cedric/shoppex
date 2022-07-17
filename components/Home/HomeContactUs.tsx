13d320b7df93490d4dd20d6f494d53f8b618ed3d
Mutesa-Cedric <mutesacedric@gmail.com>
2022-07-17 23:29:54 +0200

    update : homepage basic

--- components/Home/HomeContactUs.tsx
+++ components/Home/HomeContactUs.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function HomeContactUs() {
   return (
-    <div>HomeContactUs</div>
+    <div className="flex justify-between w-full md:flex-row px-[150px] pt-8 pb-24 flex-col">
+      <div className='flex flex-col space-y-8 w-2/4'>
+        <h1 className='font-bold text-2xl'>Contact us</h1>
+        <p className='font-bold text-gray-500 leading-7'>Something to tell us? <br /> Fell free to contact us , we&apos;ll be  open and happy <br /> to receive your idea and consider it. </p>
+      </div>
+      <div className='w-2/4 relative' >
+        <form className="flex flex-col space-y-8 w-full pl-24">
+          <input type="email" placeholder='email address'
+            className=' rounded-3xl h-12 z-10 px-5 focus:outline-none focus:border focus:border-primary' />
+          <textarea name="message" id="" cols={30} rows={8} placeholder="Message"
+            className=' pl-5 pt-3 rounded-3xl z-10 focus:outline-none focus:border focus:border-primary' />
+          <button className='self-end bg-[#FF8A00] text-white px-10 font-bold py-[5px] rounded-3xl' type='button'>
+          <span>send</span>  
+          </button>
+        </form>
+        <div className="absolute bottom-0 top-0 w-full h-full border"
+          style={{ background: ' linear-gradient(92.3deg, rgba(255, 138, 0, 0.25) -4.35%, rgba(217, 217, 217, 0) 97.96%);', filter: "blur(30px)", transform: "rotate(-180deg)" }}></div>
+      </div>
+    </div>
   )
 }
 
--- components/Home/HomeSecondBanner.tsx
+++ components/Home/HomeSecondBanner.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
+const firstDots = new Array(80).fill(1);
+const secondDots = new Array(110).fill(1);
 function HomeSecondBanner() {
   return (
-    <div>HomeSecondBanner</div>
+    <div className='w-full h-[480px] flex flex-col items-center justify-center bg-[#7F485A] space-y-10 relative'>
+      <p className="text-white font-bold text-2xl max-w-md text-center">Don&apos;t be late! Make your shopping perfect and enjoyable with us.</p>
+      <button className='text-primary border-2 border-primary rounded-[20px] px-6 py-2  font-bold hover:bg-primary hover:text-white transition-all duration-500'>
+        Get started now
+      </button>
+      <div className='absolute bottom-0 left-0 grid grid-cols-8 gap-2'>
+        {
+          firstDots.map(dot => {
+            return <div className="w-3 h-3 bg-[#A75C3D] rounded-full"></div>
+          })
+        }
+      </div>
+      <div className='absolute right-0 -top-16 grid grid-cols-11 gap-2'>
+        {
+          secondDots.map(dot => {
+            return <div className="w-3 h-3 bg-[#D9D9D980]/50 rounded-full"></div>
+          })
+        }
+      </div>
+    </div>
   )
 }
 
